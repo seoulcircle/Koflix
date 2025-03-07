991274e7a6bb2dcbf05c2838e7366f9a10402fc7
seoulcircle <77052964+seoulcircle@users.noreply.github.com>
2025-03-07 23:38:46 +0900

    라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
 
 function App() {
-  return <>hello</>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
